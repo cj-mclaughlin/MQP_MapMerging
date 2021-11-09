60d614de7f9d6cf3438579f790028471a435d302
Connor Mclaughlin <cjmclaughlin@wpi.edu>
2021-11-09 01:45:16 -0500

    evaluation script complete

--- hough_sift_mapmerge.py
+++ hough_sift_mapmerge.py
@@ -1,8 +1,8 @@
 import numpy as np
-import random
 import matplotlib.pyplot as plt
-import tensorflow as tf
 import cv2
+import scipy
+from time import time
 
 BASE_PATH = "Data/Mercer/"
 
@@ -34,6 +34,10 @@ def load_mercer_map(txt_path, dtype=np.uint8):
         map[map == UNKNOWN] = UNKNOWN_FLOAT
     return map
 
+def get_training_sample(map_filename):
+    map = load_mercer_map(map_filename)
+    aug_map, labels = augment_map(map)
+    return map, aug_map
 
 def show_samples():
     for map_idx in range(len(TRAIN_FILENAMES)):
@@ -48,7 +52,7 @@ def apply_warp(map, M, fill=UNKNOWN):
     map_warp = cv2.warpAffine(src=map_warp, M=M, dsize=(y, x), flags=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT, borderValue=fill)
     return map_warp
 
-def augment_map(map, shift_limit=0.2, rotate_limit=45, fill=UNKNOWN):
+def augment_map(map, shift_limit=0.1, rotate_limit=360, fill=UNKNOWN):
     """
     apply set of random image augmentation to map image
     return augmented map, as well as parameters for augmentation
@@ -56,7 +60,7 @@ def augment_map(map, shift_limit=0.2, rotate_limit=45, fill=UNKNOWN):
     x, y = map.shape[0], map.shape[1]
     center = y/2, x/2
     angle = np.random.uniform(low=-rotate_limit, high=rotate_limit)
-    angle = 45  # hard code for consistency
+    # angle = 45  # hard code for consistency
     M_rotation = cv2.getRotationMatrix2D(center=center, angle=angle, scale=1.0)
     rotated_map = apply_warp(map, M_rotation, fill=fill)
     shift_prop_x = np.random.uniform(low=-shift_limit, high=shift_limit)
@@ -104,6 +108,8 @@ def hough_map_transform(map):
     edge_map[edge_map == OCCUPIED] = 1  # temp
     edge_map[edge_map == UNKNOWN] = 0
     edge_map[edge_map == FREE] = 0
+    # plt.imshow(edge_map, cmap="gray")
+    # plt.show()
     h, theta, d = hough_line(edge_map, theta=tested_angles)
     return h, theta, d
 
@@ -120,14 +126,17 @@ def extract_local_maximums(signal, num):
     return np.argpartition(signal, -num)[-num:]
 
 def axis_spectrum(axis, map):
-    # edge_map = np.copy(map)
+    edge_map = np.copy(map)
     # edge_map[edge_map == OCCUPIED] = 1  # temp
-    # edge_map[edge_map == UNKNOWN] = 0
+    edge_map[edge_map == UNKNOWN] = FREE
+    edge_map = 255 - edge_map
     # edge_map[edge_map == FREE] = 0
-    spect = np.sum(map, axis=axis)
-    return spect / np.linalg.norm(spect)
+    # edge_map[edge_map == 1] = 255
+    spect = np.sum(edge_map, axis=axis)
+    return spect / np.max(spect)
+    # return spect / np.linalg.norm(spect)
 
-def compute_hypothesis(map1, map2, num):
+def hough_mapmerge(map1, map2, num=4):
     """
     produces best possible accuracy merges given two maps
     """
@@ -140,75 +149,38 @@ def compute_hypothesis(map1, map2, num):
     HS_M1 = hough_spectrum_calculation(map1)
     HS_M2 = hough_spectrum_calculation(map2)
 
-    # TODO debug
-    render_hough_spectrum(HS_M1, map1)
-    render_hough_spectrum(HS_M2, map2)
-
     CC_M1_M2 = FFT_circular_cross_correlation(HS_M1, HS_M2)
     local_max = extract_local_maximums(CC_M1_M2, num)
     
     SX_M1 = axis_spectrum(0, map1)
     SY_M1 = axis_spectrum(1, map1)
-    map3 = None
 
+    map3 = None
     for rot in local_max:
         M_rotation = cv2.getRotationMatrix2D(center=center, angle=-rot, scale=1.0)
         map3 = apply_warp(map2, M_rotation, fill=UNKNOWN)  # dont cheat lol
-
-        plt.imshow(map3, cmap="gray")
-        plt.title("candidate map3 after just rotation")
-        plt.show()
-
         SX_M3 = axis_spectrum(0, map3)
         SY_M3 = axis_spectrum(1, map3)
 
-        CC_M1_M3_X = cross_correlation(SX_M1, SX_M3)
-        CC_M1_M3_Y = cross_correlation(SY_M1, SY_M3)
+        CCX = scipy.signal.correlate(SX_M1, SX_M3, method="fft")
+        CCY = scipy.signal.correlate(SY_M1, SY_M3, method="fft")
         
-        best_dx = extract_local_maximums(CC_M1_M3_X, 1)[0]
-        best_dy = extract_local_maximums(CC_M1_M3_Y, 1)[0]
+        best_dx = (CCX.shape[0] + (1 - SX_M1.shape[0]) - np.argmax(CCX))
+        best_dy = (CCY.shape[0] + (1 - SY_M1.shape[0]) - np.argmax(CCY))
         
         M_translation = np.float32([
-            [1, 0, best_dx],
-            [0, 1, best_dy]
+            [1, 0, -best_dx],
+            [0, 1, -best_dy]
         ])
         cand_map = apply_warp(map3, M_translation, fill=UNKNOWN)
         acpt = accept(map1, cand_map)
         
-        print(best_dx, best_dy, rot, acpt)
-        
         if acpt > best_acpt:
             best_acpt = acpt
             best_map = cand_map
-            best_params = (best_dx, best_dy, rot)
+            # best_params = (best_dx, best_dy, rot)
             
-    return best_map, best_params
-
-"""### SIFT Based"""
-
-from tensorflow.python.keras.utils import data_utils
-class CustomDataGen(data_utils.Sequence):
-    def __init__(self, filenames, batch_size, shuffle=True):
-        self.maps = [load_mercer_map(filename) for filename in filenames]
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.n = len(filenames)
-
-    def on_epoch_end(self):
-        if self.shuffle:
-            random.shuffle(self.maps)
-
-    def __getitem__(self, index):
-        map = self.maps[index]
-        aug_map, labels = augment_map(map)
-        # map_3d = np.expand_dims(map, axis=-1)
-        # aug_map_3d = np.expand_dims(aug_map, axis=-1)
-        # maps = np.concatenate([map_3d, aug_map_3d], axis=-1)
-        return map, aug_map, labels
-    
-    def __len__(self):
-        return self.n // self.batch_size
-
+    return best_map
 
 def blur_map(map):
     src = np.copy(map)
@@ -220,30 +192,57 @@ def sift_mapmerge(map1, map2):
     sift = cv2.SIFT_create()
     kp1, desc1 = sift.detectAndCompute(map1, None)
     kp2, desc2 = sift.detectAndCompute(map2, None)
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    index_params = dict(algorithm = 0, trees = 5)
     search_params = dict(checks=50)
-    flann = cv2.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(desc1,desc2,k=2)
-    good_matches = []
-    for m, n in matches:
-        if m.distance < 0.7 * n.distance:
-            good_matches.append(m)
+    try:
+        flann = cv2.FlannBasedMatcher(index_params,search_params)
+        matches = flann.knnMatch(desc1,desc2,k=2)
+        good_matches = []
+        for m, n in matches:
+            # lowes ratio
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+    except:
+        # failed merge
+        return np.ones_like(map2) * UNKNOWN
     src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches ]).reshape(-1,1,2)
     dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
 
     M, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 5.0)
+    return apply_warp(map2, M[:2])
 
-    return M[:2]  # should return 2x3 homography matrix, (last row is 0 0 1 regardless)
+def orb_mapmerge(map1, map2):
+    map1, map2 = blur_map(map1), blur_map(map2)
+    orb = cv2.ORB_create(nfeatures=250)
+    kp1, desc1 = orb.detectAndCompute(map1, None)
+    kp2, desc2 = orb.detectAndCompute(map2, None)
+    index_params = dict(algorithm = 6, trees = 6, key_size=12, multi_probe_level = 1)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(desc1,desc2,k=2)
+    good_matches = []
+    try:
+        for m, n in matches:
+            # use slightly higher ratio for ORB
+            if m.distance < 0.8 * n.distance:
+                good_matches.append(m)
+    except:
+        # failed merge
+        return np.ones_like(map2) * UNKNOWN
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 5.0)
+    return apply_warp(map2, M[:2])
 
 def agr(map1, map2):
     agree = np.count_nonzero((map1 == OCCUPIED) & (map2 == OCCUPIED))
-    agree += np.count_nonzero((map1 == FREE) & (map2 == OCCUPIED))
+    agree += np.count_nonzero((map1 == FREE) & (map2 == FREE))
     return agree    
 
 def dis(map1, map2):
     disagree = np.count_nonzero((map1 == OCCUPIED) & (map2 == FREE))
-    disagree = np.count_nonzero((map1 == FREE) & (map2 == OCCUPIED))
+    disagree += np.count_nonzero((map1 == FREE) & (map2 == OCCUPIED))
     return disagree
 
 def accept(map1, map2):
@@ -252,33 +251,54 @@ def accept(map1, map2):
     iou = 0 if a == 0 else (a / (a+d))
     return iou
 
-
 if __name__ == "__main__":
     # trying out SIFT...
-    train_gen = CustomDataGen(filenames=TRAIN_FILENAMES, batch_size=1)
-    map1, map2, params = train_gen[0]
+    filename1 = TRAIN_FILENAMES[0]
+    filename2 = TRAIN_FILENAMES[5]
+    filename3 = TRAIN_FILENAMES[6]
+    filename4 = TEST_FILENAMES[0]
 
-    plt.imshow(map1, cmap="gray")
-    plt.title("Original")
-    plt.show()
-    plt.imshow(map2, cmap="gray")
-    plt.title("Rotated")
-    plt.show()
+    maps = [filename1, filename2, filename3, filename4]
 
-    map_hough, shift_params = compute_hypothesis(map1, map2, 4)
-    plt.imshow(map_hough, cmap="gray")
-    plt.title("Recovered - Hough")
-    plt.show()
-    print("Acceptance index for hough:", accept(map1, map_hough))
-
-    # print(shift_params)
-    sift_M = sift_mapmerge(map1, map2)
-    map_sift = apply_warp(map2, sift_M)
-    plt.imshow(map_sift, cmap="gray")
-    plt.title("Recovered - SIFT")
-    plt.show()
-    print("Acceptance index for SIFT:", accept(map1, map_sift))
-
-    print("GT shift parameters:", params)
-    print("Parameters found for hough:", shift_params)
-    print("Parameters found for SIFT:", sift_M)
+    N_ITERS = 1000
+    SIFT_RESULTS = [[], [], [], []]
+    SIFT_TIMES = [[], [], [], []]
+    ORB_RESULTS = [[], [], [], []]
+    ORB_TIMES = [[], [], [], []]
+    HOUGH_RESULTS = [[], [], [], []]
+    HOUGH_TIMES = [[], [], [], []]
+    for m_idx in range(len(maps)):
+        for i in range(N_ITERS):
+            map1, map2 = get_training_sample(maps[m_idx])
+            plt.imsave(f"Data/Images/map{m_idx}.png", map1, cmap="gray")
+            # sift
+            sift_start = time()
+            sift_map = sift_mapmerge(map1, map2)
+            sift_end = time()
+            sift_elapsed = sift_start - sift_end
+            SIFT_RESULTS[m_idx].append(accept(map1, sift_map))
+            SIFT_TIMES[m_idx].append(sift_elapsed)
+            # hough 
+            hough_start = time()
+            hough_map = hough_mapmerge(map1, map2)
+            hough_end = time()
+            hough_elapsed = hough_start - hough_end
+            HOUGH_RESULTS[m_idx].append(accept(map1, hough_map))
+            HOUGH_TIMES[m_idx].append(hough_elapsed)
+            # orb 
+            orb_start = time()
+            orb_map = orb_mapmerge(map1, map2)
+            orb_end = time()
+            orb_elapsed = orb_start - orb_end
+            ORB_RESULTS[m_idx].append(accept(map1, orb_map))
+            ORB_TIMES[m_idx].append(orb_elapsed)
+    
+    SIFT_MU, SIFT_STD, SIFT_TIME = np.mean(SIFT_RESULTS, axis=1), np.std(SIFT_RESULTS, axis=1), np.mean(SIFT_TIMES, axis=1)
+    print("(Average Acc, Std Acc, Average Time) per map: (SIFT)")
+    print(SIFT_MU, SIFT_STD, SIFT_TIME)
+    ORB_MU, ORB_STD, ORB_TIME = np.mean(ORB_RESULTS, axis=1), np.std(ORB_RESULTS, axis=1), np.mean(ORB_TIMES, axis=1)
+    print("(Average Acc, Std Acc, Average Time) per map: (ORB)")
+    print(ORB_MU, ORB_STD, ORB_TIME)
+    HOUGH_MU, HOUGH_STD, HOUGH_TIME = np.mean(HOUGH_RESULTS, axis=1), np.std(HOUGH_RESULTS, axis=1), np.mean(HOUGH_TIMES, axis=1)
+    print("(Average Acc, Std Acc, Average Time) per map: (HOUGH)")
+    print(HOUGH_MU, HOUGH_STD, HOUGH_TIME)
